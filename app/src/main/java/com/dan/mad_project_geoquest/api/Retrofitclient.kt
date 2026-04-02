ec70d217ae4c7c388087ca0c4d737ce949fc9fa1
DAN0HAK0 <144177845+DAN0HAK0@users.noreply.github.com>
2026-04-02 21:24:18 +0100

    wrong code was used, followed the module given code

--- app/src/main/java/com/dan/mad_project_geoquest/api/Retrofitclient.kt
+++ app/src/main/java/com/dan/mad_project_geoquest/api/Retrofitclient.kt
@@ -1,12 +1,30 @@
 package com.dan.mad_project_geoquest.api
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+const val API_KEY = "577p2m"
+const val BASE_URL = "https://mark0s.com/geoquest/v1/"
+
 object RetrofitClient {
+
+    // OkHttp logging interceptor — exactly as shown in course API.pdf
+    private val logger = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BASIC
+    }
+
+    private val client by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .build()
+    }
+
     val instance: GeoQuestApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(GeoQuestApiService::class.java)
